8e560d2913d151099e0c95c32f2e2a95abcd0982
soy eddy <soyeddy.email.com>
2025-02-08 14:00:40 -0600

    pruebas en componente app.tsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest" />
+import {  defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+import path from "path"
+
+
 export default defineConfig({
-  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  plugins: [
+    react(),
+  
+  ],
+
+  test: {
+    // @ts-expect-error - vite-test-runner types are not available
+    global: true,
+    environment: 'jsdom',
+    setupFiles: ['./setupTest.ts'],
+    coverage: {
+      provider: 'v8'
+    }
+  },
 })
